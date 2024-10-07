ccd7030d2d2d19d866a230f84b252bfd45695a50
Jonas Birmé <jonas.birme@eyevinn.se>
2024-10-07 13:18:00 +0200

    fix: need to use port 8080

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -39,5 +39,8 @@ awk '
 	{ print }
 ' "wp-config-docker.php" > wp-config.php
 chown "www-data:www-data" wp-config.php
-
+sed -e 's/Listen 80/Listen 8080/' < /etc/apache2/ports.conf > /etc/apache2/ports.conf.new
+mv /etc/apache2/ports.conf.new /etc/apache2/ports.conf
+sed -e 's/VirtualHost \*:80/VirtualHost \*:8080/' < /etc/apache2/sites-available/000-default.conf > /etc/apache2/sites-available/000-default.conf.new
+mv /etc/apache2/sites-available/000-default.conf.new /etc/apache2/sites-available/000-default.conf
 exec "$@"
